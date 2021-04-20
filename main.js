9c26075a56a21d44329c2d2561e928151e9cd3e9
Rajan-120 <78260275+Rajan-120@users.noreply.github.com>
2021-04-20 20:51:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function show1() {
+    document.getElementById("item1").innerHTML = "I just sprayed water with a fine spray bottle and took the the camera too close to the flower without any zoom because in mobiles zoom makes a photograph blurry and makes the quality poor.";
+}
+
+function hide1() {
+    document.getElementById("item1").innerHTML = "";
+}
+
+function show2() {
+    document.getElementById("item2").innerHTML = "In this photograph its feeling like passing through a leaf and reaching to the plants. I have used this concept for this photograph.";
+}
+
+function hide2() {
+    document.getElementById("item2").innerHTML = "";
+}
